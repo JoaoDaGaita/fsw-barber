3b04cefcda090f5997f717e6851ed124c2cb24df
joaodagaita <joaovsilva70@outlook.com>
2024-08-07 00:15:12 -0300

    chore: add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import { Button } from "./components/ui/button";
+
 export default function Home() {
   return (
-    <div className="bg-black text-lg flex flex-col h-screen w-full">
-    </div>
+    <Button>Test</Button>
   )
 }
