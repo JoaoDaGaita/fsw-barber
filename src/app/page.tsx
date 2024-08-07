e66d4d92ff08d4fcef63e7ec295f137ce0239644
joaodagaita <joaovsilva70@outlook.com>
2024-08-07 10:55:05 -0300

    feat: add Header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import { SearchIcon } from "lucide-react";
+import { Header } from "./components/header";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button>Test</Button>
-  )
+    <>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, João</h2>
+        <p>Quarta, 7 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua Busca..." />
+          <Button>
+            <SearchIcon color="white" />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores barbeiros"
+            src="/banner.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
