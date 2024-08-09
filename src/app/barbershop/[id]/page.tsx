ccf570187e4079340749ac29ecdf3b277fa17a91
joaodagaita <joaovsilva70@outlook.com>
2024-08-09 10:27:37 -0300

    feat: ensure barbershop services are shown on barbershop page

--- src/app/barbershop/[id]/page.tsx
+++ src/app/barbershop/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { ServiceItem } from "@/app/components/service-item";
 import { Button } from "@/app/components/ui/button";
 import { db } from "@/app/lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
@@ -16,11 +17,16 @@ export default async function BarberShopPage({ params }: BarberShopPageProps) {
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   });
 
   if (!barbershop) {
     return notFound();
   }
+
+  console.log(barbershop);
 
   return (
     <div>
@@ -68,6 +74,15 @@ export default async function BarberShopPage({ params }: BarberShopPageProps) {
       <div className="space-y-3 border-b border-solid p-5">
         <h2 className="font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop.description}</p>
+      </div>
+
+      <div className="space-y-3 p-5">
+        <h2 className="font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/app/components/service-item.tsx
@@ -0,0 +1,40 @@
+import type { BarbershopService } from "@prisma/client";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+
+interface ServiceItemProps {
+  service: BarbershopService;
+}
+export function ServiceItem({ service }: ServiceItemProps) {
+  return (
+    <Card>
+      <CardContent className="flex items-center gap-2 p-3">
+        <div className="relative min-h-[110px] min-w-[110px]">
+          <Image
+            src={service.imageUrl}
+            alt={service.name}
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="space-y-3">
+          <h3 className="text-sm font-semibold">{service.name}</h3>
+          <p className="text-sm text-gray-400">{service.description}</p>
+
+          <div className="flex items-center justify-between">
+            <p className="text-sm font-bold text-primary">
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(Number(service.price))}
+            </p>
+            <Button variant="secondary" className="gap-4" size="sm">
+              Reservar
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
