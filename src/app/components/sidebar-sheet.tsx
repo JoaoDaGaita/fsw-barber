b8028a71d1000de6796439eb5606225181d3babf
joaodagaita <joaovsilva70@outlook.com>
2024-08-13 15:39:15 -0300

    chore: add next auth setup

--- src/app/components/sidebar-sheet.tsx
+++ src/app/components/sidebar-sheet.tsx
@@ -1,9 +1,23 @@
-import { Link, HomeIcon, CalendarIcon, LogOutIcon } from "lucide-react";
+import {
+  Link,
+  HomeIcon,
+  CalendarIcon,
+  LogOutIcon,
+  LogInIcon,
+} from "lucide-react";
 import { quickSearchOptions } from "../_constants/search";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
-import { Avatar, AvatarImage } from "./ui/avatar";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "./ui/dialog";
+//import { Avatar, AvatarImage } from "./ui/avatar";
 
 export function SidebarSheet() {
   return (
@@ -12,18 +26,38 @@ export function SidebarSheet() {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image alt="" src="/google.svg" width={18} height={18} />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage
             className="object-cover"
             src="https://plus.unsplash.com/premium_photo-1674180786953-4223a4208d9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D"
           />
-        </Avatar>
+        </Avatar> */}
 
-        <div>
+        {/* <div>
           <p className="font-bold">Cleiton Conservani</p>
           <p className="text-xs">cleitoncon@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
